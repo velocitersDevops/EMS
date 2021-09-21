19cbee24131efa7c04607c903bb7b98331a3088b
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-09-21 17:12:45 +0530

    add changes of some servlet by nitesh

--- src/javasource/com/velociter/ems/controller/LogoutServlet.java
+++ src/javasource/com/velociter/ems/controller/LogoutServlet.java
@@ -9,20 +9,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
-
-public class LogoutServlet extends HttpServlet
-{
-  @Override
-protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-  {
-	  HttpSession session = request.getSession();
-      session.invalidate(); 
-      RequestDispatcher rd=request.getRequestDispatcher("Login.jsp");
-      rd.forward(request, response);
-      response.setHeader("Cache-Control", "no-cache");
-      response.setHeader("Cache-Control", "no-store");
-      response.setHeader("Pragma", "no-cache");
-      response.setDateHeader("Expires", 0);
-  }
+public class LogoutServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.invalidate();
+		RequestDispatcher rd = request.getRequestDispatcher("Login.jsp");
+		rd.forward(request, response);
+		response.setHeader("Cache-Control", "no-cache");
+		response.setHeader("Cache-Control", "no-store");
+		response.setHeader("Pragma", "no-cache");
+		response.setDateHeader("Expires", 0);
+	}
 }
