fdf004fa4777553b6b6f01d5126a3f6b83ea933e
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-10-11 18:36:47 +0530

    add some changes of code by nitesh

--- src/javasource/com/velociter/ems/model/Project.java
+++ src/javasource/com/velociter/ems/model/Project.java
@@ -22,43 +22,71 @@ public class Project {
 	private String startDate;
 	@Column(name = "ENDDATE")
 	private String endDate;
+	@Column(name = "CREATIONDATE")
+	private String creationDate;
+	@Column(name = "LASTMODIFIED_DATE")
+	private String lastModifiedDate;
+
+	public String getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(String creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public String getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(String lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
 
 	public int getProjectId() {
 		return projectId;
 	}
+
 	public void setProjectId(int projectId) {
 		this.projectId = projectId;
 	}
+
 	public String getProjectName() {
 		return projectName;
 	}
+
 	public void setProjectName(String projectName) {
 		this.projectName = projectName;
 	}
+
 	public String getProjectDescription() {
 		return projectDescription;
 	}
+
 	public void setProjectDescription(String projectDescription) {
 		this.projectDescription = projectDescription;
 	}
+
 	public String getStartDate() {
 		return startDate;
 	}
+
 	public void setStartDate(String startDate) {
 		this.startDate = startDate;
 	}
+
 	public String getEndDate() {
 		return endDate;
 	}
+
 	public void setEndDate(String endDate) {
 		this.endDate = endDate;
 	}
+
 	@Override
 	public String toString() {
 		return "Project [projectId=" + projectId + ", projectName=" + projectName + ", projectDescription="
 				+ projectDescription + ", startDate=" + startDate + ", endDate=" + endDate + "]";
 	}
-	
-	
-	
+
 }
